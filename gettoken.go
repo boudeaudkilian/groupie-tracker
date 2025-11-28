bb4574744c83c7119300c7c5999aa5ba6d1b5720
dewen <ewen.denoual@ynov.com>
2025-11-28 09:34:37 +0100

    add fct multitoken for case with multi arg like members

--- gettoken.go
+++ gettoken.go
@@ -35,7 +35,40 @@ func gettoken(buffer, arg string) string {
 	return ret
 }
 
-func main() {
+func getmultitoken(buffer, arg string) []string {
+	box := ""
+	var ret []string
+	isword := 0
+	isgoodword := 0
+
+	for _, v := range buffer {
+		if isgoodword == 1 {
+			if v == ']'{
+				break
+			}
+			if v == ',' {
+				ret = append(ret, box)
+				box = ""
+			} else if v != '"' {
+				box += string(v)
+			}
+		} else if isgoodword == 2 && v == '[' {
+			isgoodword = 1
+			box = ""
+		} else if v == '"' {
+			isword = (isword + 1) % 2
+			box = ""
+		} else if isword == 1 {
+			box += string(v)
+			if box == arg {
+				isgoodword = 2
+			}
+		}
+	}
+	return ret
+}
+
+func main() {  //test main for fct
 	url := "https://groupietrackers.herokuapp.com/api/artists"
 
 	req, _ := http.NewRequest("GET", url+"/1", nil)
@@ -45,6 +78,8 @@ func main() {
 	body, _ := io.ReadAll(res.Body)
 
 	tok := gettoken(string(body), "name")
-
 	fmt.Println(tok)
+
+	tok2 := getmultitoken(string(body), "members")
+	fmt.Println(tok2)
 }

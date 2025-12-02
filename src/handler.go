f240735926e8d47ee6e15eee17611c025c916b1c
dewen <ewen.denoual@ynov.com>
2025-12-02 09:11:51 +0100

    [Ajout] new main and handler at right position

---
+++ src/handler.go
@@ -0,0 +1,22 @@
+package groupie
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
+
+func AccueilHandler(w http.ResponseWriter, r *http.Request) {
+    funcMap := template.FuncMap{
+        "LoadGroup": LoadGroup,
+    }
+
+    tmpl := template.Must(
+        template.New("accueil.html").Funcs(funcMap).
+            ParseFiles("template/accueil.html"),
+    )
+    data := LoadGroupResum()
+    if err := tmpl.Execute(w, data); err != nil {
+        log.Println("❌ Erreur template:", err)
+    }
+}

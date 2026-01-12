e87d94a049d1c354b80af397f97062a1463c104f
lerwan <erwan.lievre@ynov.com>
2026-01-12 08:01:19 +0100

    [DEBUG] résolution de problème de fonctionnement des pages

--- main.go
+++ main.go
@@ -1,21 +1,16 @@
 package main
 
 import (
-	"groupie/src"
+	groupie "groupie/src"
 	"log"
 	"net/http"
 )
 
-func AccueilHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	_, _ = w.Write([]byte("Bonjour et bienvenue sur Groupie Tracker"))
-}
-
 func main() {
 	http.HandleFunc("/", groupie.AccueilHandler)
-	http.HandleFunc("/grppage/", groupie.PageGroupHandler)
+	http.HandleFunc("/group", groupie.PageGroupHandler)
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 
-	log.Println(" Bonjour et bienvenus sur notre serveur Groupie Tracker il est lancé sur http://localhost:8080")
+	log.Println("Bonjour et bienvenus sur notre serveur Groupie Tracker: http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
